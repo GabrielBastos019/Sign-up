c40e78acf2c1955b1cac7b975f81dc3a1230a343
GabrielBastos019 <gabrieldev019@gmail.com>
2023-09-29 00:50:24 -0300

    Deixando o codigo mais legivel

--- src/js/index.js
+++ src/js/index.js
@@ -6,17 +6,25 @@ var emailDigitado = document.querySelector("#emailDigitado");
 
 btn.addEventListener("click", () => {
   if (input.validity.valid) {
-    container.style.display = "none";
-    sucess.style.display = "flex";
-    sucess.style.flexDirection = "column";
-    emailDigitado.innerHTML = input.value;
-    input.value = "";
-    input.style.borderColor = "grey";
-    input.style.backgroundColor = "white";
+    aparecer()
   } else {
     return;
   }
 });
+
+function aparecer() {
+  var container = document.querySelector("#container");
+  var input = document.querySelector("#email");
+  var sucess = document.querySelector(".sucess");
+  var emailDigitado = document.querySelector("#emailDigitado");
+  container.style.display = "none";
+  sucess.style.display = "flex";
+  sucess.style.flexDirection = "column";
+  emailDigitado.innerHTML = input.value;
+  input.value = "";
+  input.style.borderColor = "grey";
+  input.style.backgroundColor = "white";
+}
 
 var btnSucess = document.querySelector("#btn-sucess");
 
@@ -28,19 +36,3 @@ btnSucess.addEventListener("click", () => {
     return;
   }
 });
-
-
-var corpo = document.querySelector('html')
-var img = document.querySelector('#ilustracao')
-var divImg = document.querySelector('#div_image')
-
-if(corpo.clientWidth < 960){
-  img.classList.remove('.height')
-  divImg.classList.remove('.height')
-}else{
-  console.log('erro');
-}
-
-console.log(corpo);
-
-
